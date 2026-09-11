812f40c2d97eafe5d8521a98a33c48b8489d0d1d
Robby Cochran <rcochran@redhat.com>
2026-09-11 10:50:21 -0700

    test: satisfy lint for workflow pin check

--- test/pr_review_test.go
+++ test/pr_review_test.go
@@ -201,7 +201,7 @@ func isSHA(value string) bool {
 		return false
 	}
 	for _, r := range value {
-		if !((r >= '0' && r <= '9') || (r >= 'a' && r <= 'f')) {
+		if (r < '0' || r > '9') && (r < 'a' || r > 'f') {
 			return false
 		}
 	}
